3b0f0180814f1c192910c51b3b43261bc00b763b
Shubham Karande <shubhamkarande0501@gmail.com>
2022-01-17 21:37:06 +0530

    User can create message

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,62 +1,59 @@
 
-from channels.db import database_sync_to_async
+from inspect import _void
 from channels.generic.websocket import  AsyncWebsocketConsumer
-
-from chat.models import ChatMessage
+from asgiref.sync import sync_to_async
 from chat.serializers import ChatMessageSerializer
-from users.models import Project, User
-from users.serializers import ProjectSerializer
-from utils.common import serialize, serialize_async
+from users.models import Project
+from users.serializers import UserSerializer
+from utils.common import (
+  get_messages, 
+  get_project,
+  get_user, 
+  get_user_in_project, 
+  serialize_async
+)
+from channels.db import database_sync_to_async
+from .models import ChatMessage
 
 import json
-
-@database_sync_to_async
-def get_project(pk: int) -> Project:
-  return Project.objects.filter(id=pk).first()
-
-@database_sync_to_async
-def get_user_in_project(email: str, project: Project) -> bool:
-  return User.objects.filter(email=email).first() in project.members.all()
-
-@database_sync_to_async
-def get_messages(project: Project) -> list:
-  return ChatMessage.objects.filter(project=project).order_by('-date_sent')
 
 class ChatConsumer(AsyncWebsocketConsumer):
 
   project = None
+  user = None
 
   async def connect(self):
-    pk: int = self.scope['url_route']['kwargs']['pk']
-    params: str = self.scope['query_string'].decode("utf-8")
-    email: str = params.split('=')[1]
-    self.project: Project = await get_project(pk)
-    user_in_project: bool = await get_user_in_project(email, self.project)
+    try:
+      pk: int = self.scope['url_route']['kwargs']['pk']
+      params: str = self.scope['query_string'].decode("utf-8")
+      email: str = params.split('=')[1]
+      await self.accept()
+      self.project = await self.get_project(pk)
+      user_in_project: bool = await get_user_in_project(email, self.project)
+      self.user = await get_user(email)
+      if not user_in_project:
+        print('Not in project')
+        await self.disconnect()  
+        await self.send(f'{email} is not allowed to message in {str(self.project)}')
 
-    if not user_in_project:
-      print('Not in project')
-      await self.close()  
-      await self.send(f'{email} is not allowed to message in {str(self.project)}')
+      self.room_name = "Room name!"
+      self.room_group_name = 'chat_%s' % pk
 
-    self.room_name = self.project.name
-    self.room_group_name = 'chat_%s' % pk
-
-    await self.accept()
-    await self.channel_layer.group_add(
-      self.room_group_name,
-      self.channel_name
-    )
-
-    await self.channel_layer.group_send(
-      self.room_group_name,
-      {
-        'type': "send_messages"
-      }
-    )
+      await self.channel_layer.group_add(
+        self.room_group_name,
+        self.channel_name
+      )
+      await self.channel_layer.group_send(
+        self.room_group_name,
+        {
+          'type': "send_messages"
+        }
+      )
+    except Exception as e:
+      print(e)
 
   async def send_messages(self, event):
     # project = event['project']
-    print(event)
     messages = await get_messages(self.project)
     await self.send(text_data=json.dumps({
       "messages": await serialize_async(messages, ChatMessageSerializer)
@@ -70,13 +67,15 @@ class ChatConsumer(AsyncWebsocketConsumer):
       {
         "type": "chat_message",
         "message": message
+        
       }
     )
 
   async def chat_message(self, event):
     message = event['message']
+    await self.create_message(message)
     await self.send(text_data=json.dumps({
-      'message': message
+      'message': message,
     }))
 
   async def disconnect(self, close_code):
@@ -85,4 +84,19 @@ class ChatConsumer(AsyncWebsocketConsumer):
       self.room_group_name,
       self.channel_name
     )
+
+  @database_sync_to_async
+  def get_project(self, pk: int) -> Project:
+    try:
+      return Project.objects.filter(id=pk).first()
+    except Exception as e:
+      print(e)
+
+  @database_sync_to_async
+  def create_message(self, text_message):
+    try:
+      message = ChatMessage(project=self.project, user=self.user, message=text_message)
+      message.save()
+    except Exception as e:
+      print(e)
   
--- utils/common.py
+++ utils/common.py
@@ -1,9 +1,11 @@
 from channels.db import database_sync_to_async
+from django.http.response import FileResponse
 from rest_framework.response import Response
 from rest_framework import status 
 from rest_framework_simplejwt.tokens import RefreshToken
+from chat.models import ChatMessage
 
-from users.models import User
+from users.models import Project, User
 from users.serializers import UserSerializer
 
 
@@ -23,14 +25,6 @@ def serialize(model_list, Serializer):
   serialized = [Serializer(model).data for model in model_list]
   return serialized
 
-@database_sync_to_async
-def serialize_async(model_list, Serializer):
-  serialized = [Serializer(model).data for model in model_list]
-  for message in serialized:
-    user = User.objects.filter(id=message['user']).first()
-    message['user'] = UserSerializer(user).data
-  return serialized
-
 def update(data, instance, Serializer) -> Response:
   print(data)
   serializer = Serializer(instance, data=data, partial=True)
@@ -40,3 +34,27 @@ def update(data, instance, Serializer) -> Response:
     serializer.save()
     return Response(serializer.data, status=status.HTTP_200_OK)
   return Response(serializer.error, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+  
+@database_sync_to_async
+def serialize_async(model_list, Serializer):
+  serialized = [Serializer(model).data for model in model_list]
+  for message in serialized:
+    user = User.objects.filter(id=message['user']).first()
+    message['user'] = UserSerializer(user).data
+  return serialized
+
+@database_sync_to_async
+def get_project(pk: int) -> Project:
+  return Project.objects.filter(id=pk).first()
+
+@database_sync_to_async
+def get_user_in_project(email: str, project: Project) -> bool:
+  return User.objects.filter(email=email).first() in project.members.all()
+
+@database_sync_to_async
+def get_messages(project: Project) -> list:
+  return ChatMessage.objects.filter(project=project).order_by('-date_sent')
+
+@database_sync_to_async
+def get_user(email: str) -> User:
+  return User.objects.filter(email=email).first()
